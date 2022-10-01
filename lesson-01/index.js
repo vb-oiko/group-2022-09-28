23c98f7be66cc0d78c11c6169addd0f22bc21672
Vasyl Boyko <vboiko0@gmail.com>
2022-10-01 13:16:45 +0300

    add lesson 01

---
+++ lesson-01/index.js
@@ -0,0 +1,21 @@
+/* console.log("Hello world");
+ console.log("Hello world");
+ console.log("Hello world"); */
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+// console.log("Hello world");
+
+// window.alert("Warning!");
+
+// let name1 = window.prompt("Please enter your name");
+// console.log(name1);
+
+let result = confirm("Are you sure you want to close it?");
+console.log(result);
