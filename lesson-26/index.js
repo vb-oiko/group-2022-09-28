9dddd6cfd565c8f2e6b62b5ccf4409b5a5d0c5da
Vasyl Boyko <vboiko0@gmail.com>
2022-12-15 10:25:10 +0200

    add lesson 26

---
+++ lesson-26/index.js
@@ -0,0 +1,48 @@
+// AJAX
+// https://en.wikipedia.org/wiki/Ajax_(programming)
+
+// HTTP
+// https://developer.mozilla.org/en-US/docs/Web/HTTP
+
+// request
+// response
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+
+// const a = fetch(`https://jsonplaceholder.typicode.com/posts/1`, {
+//   method: "POST",
+//   headers: {
+//     ["Accept-Language"]: "fr",
+//   },
+//   //   body: JSON.stringify({ id: 1 }),
+// }).then(console.log);
+
+// methods
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+
+// codes
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+// WebSockets.
+// огляд технології
+// polling
+
+// Приклади відкритих APIs.
+// https://swapi.dev/documentation
+// https://api.nasa.gov/
+// https://pokeapi.co/
+
+// https://www.freecodecamp.org/news/modules-in-javascript/
+
+// webpack
+// import module && bundling
+// tree shaking
+// es-lint
+
+import { Button as MUIButton, Table } from "mui";
+import { Button } from "my-components";
+
+import { Button, Table } from "mui";
+import * as Components from "mui";
+Components.Button;
+
+const ids = items.map(({ id }) => id);
